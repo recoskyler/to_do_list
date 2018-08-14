efd798e790ca28e0ac994f2dab73934703ecc159
Adil Atalay Hamamcioglu <recoskyler224@gmail.com>
2018-08-14 15:23:44 +0300

    Appearence tweaks

--- src/Helpers/functions.js
+++ src/Helpers/functions.js
@@ -108,30 +108,6 @@ export function tagExists(t, arr) {
     });
 }
 
-export function getByVal(v, cbid, ROT = false) {
-    let res = 0;
-    let ddlArray = [];
-    const ddl = document.getElementById(cbid);
-    for (let i = 0; i < ddl.options.length; i++) {
-        ddlArray.push(ddl.options[i].value);
-    }
-
-    const tmp = ddlArray.forEach((itm, i) => {
-        if (itm.value === v) {
-            res = i;
-            return true;
-        } else {
-            return false;
-        }
-    });
-
-    if (ROT) {
-        return res;
-    }
-
-    return ddl.options[res].text;
-}
-
 export function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
